ade8bd29ed0ccce06b238cab69d1af520dc39500
Andrew Louis <andrew@hyfen.net>
2009-02-28 18:59:24 -0500

    Basic paper listing working

--- handlers/main.py
+++ handlers/main.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+
+from models import Paper
+
 def index(*args, **kwargs):
-    return render_to_response('papers.html')
+    return render_to_response('papers.html', {"papers": Paper.all()})
